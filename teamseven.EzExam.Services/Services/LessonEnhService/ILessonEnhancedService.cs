118376a0676bf20ce083d8127fe4f8e5e79f388a
Micutran5 <cuongtranminh510@gmail.com>
2025-10-07 11:18:22 +0700

    lesson filter

--- teamseven.EzExam.Services/Services/LessonEnhService/ILessonEnhancedService.cs
+++ teamseven.EzExam.Services/Services/LessonEnhService/ILessonEnhancedService.cs
@@ -12,5 +12,14 @@ namespace teamseven.EzExam.Services.Services.LessonEnhancedService
         Task<List<LessonEnhancedResponse>> GetByQuestionIdAsync(int questionId);
         Task<List<LessonEnhancedResponse>> GetAllAsync(string? subjectId = null, bool includeQuestions = false);
 
+        Task<PagedResponse<LessonEnhancedResponse>> GetPagedAsync(
+        int? pageNumber = null,
+        int? pageSize = null,
+        string? search = null,
+        string? sort = null,
+        string? subjectId = null,   // string để khớp query từ controller
+        string? questionId = null,  // string để khớp query từ controller
+        int isSort = 0,
+        bool includeQuestions = false);
     }
 }
